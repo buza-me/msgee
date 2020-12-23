5d5dfec8ef257b69c31c3a89608afc967c8a41b1
kbuza <buza.work@gmail.com>
2020-12-23 04:33:51 +0200

    Will change configs and add scripts needed to publish a library.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # msgee
 An extended events manager
+
+## Installation - NPM
+```sh
+npm i msgee
+```
+
+## Installation - Browser
+```html
+<script src="not yet"></script>
+```
--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,28 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 export default [
-  {
-    input: 'src/main.ts',
-    output: [
-      {
-        file: 'dist/bundle.umd.js',
-        format: 'umd',
-        name: 'msgee',
-        exports: 'named',
-        sourcemap: true,
-      },
-    ],
-    plugins: [typescript(), terser()],
-  },
+  // {
+  //   input: 'src/main.ts',
+  //   output: [
+  //     {
+  //       file: 'dist/bundle.umd.js',
+  //       format: 'umd',
+  //       name: 'msgee',
+  //       exports: 'named',
+  //       sourcemap: true,
+  //     },
+  //   ],
+  //   plugins: [typescript()],
+  // },
   {
     input: 'src/browser.ts',
     output: {
       file: 'dist/bundle.browser.umd.js',
-      format: 'umd',
+      format: 'iife',
       name: 'msgee',
       exports: 'named',
       sourcemap: true,
     },
-    plugins: [typescript(), terser()],
+    plugins: [typescript({ target: 'es5' }), terser()],
   },
 ];
